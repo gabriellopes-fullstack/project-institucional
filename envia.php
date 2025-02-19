9ce0c1cc32f6fd29ff8b456149fdc0b6eaf74965
Gabriel Lopes | Front-End <gabriellrezende@gmail.com>
2025-02-19 17:52:50 -0300

    update

--- envia.php
+++ envia.php
@@ -1,16 +1,16 @@
 <?php
 
-    $nome = addslashes($_POST['nome']);
-    $email = addslashes($_POST['email']);
-    $telefone = addslashes($_POST['telefone']);
-    $mensagem = addslashes($_POST['mensagem']);
+    $nome = addslashes( $_POST['nome']);
+    $email = addslashes( $_POST['email']);
+    $telefone = addslashes( $_POST['telefone']);
+    $mensagem = addslashes( $_POST['mensagem']);
 
-    $recebedor = "gabriellrezende@gmail.com";
+    $recebedor = "gabriellrezende@gmail.com";   
     $assunto = "Coleta de dados - Contato";
 
-    $corpo = "Nome: ".$nome."email: ".$email."telefone: ".$telefone."mensagem: ".$mensagem;
+    $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone."\n"."mensagem: ".$mensagem;
 
-    $cabeca = "From: gabriellrezende@gmail.com"."Replay-to".$email."X=Mailer:PHP/".phpversion();
+    $cabeca = "From: gabriellrezende@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
 
     if(mail($recebedor, $assunto, $corpo, $cabeca)){
         echo("E-mail enviado com sucesso");
